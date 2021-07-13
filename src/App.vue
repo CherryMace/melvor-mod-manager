a229c9c4acf6a6d063b26cd648ac6bf9cf36bb8d
Chase Strackbein <chase.strackbein@gmail.com>
2021-07-13 16:29:57 -0400

    Add FontAwesome icons

--- src/App.vue
+++ src/App.vue
@@ -33,25 +33,20 @@
         text
       >
         <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <font-awesome-icon icon="external-link-alt" />
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  components: { },
 
   data: () => ({
     //
