d2bbf0b8f2f44a5b4977ad8b3913b46e57cf826d
Chase Strackbein <chase.strackbein@gmail.com>
2021-08-19 01:40:06 -0400

    Use better color for mod list selection

--- src/components/ModList.vue
+++ src/components/ModList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <v-list nav dense color="transparent">
-      <v-list-item-group @change="onChangeSelectedMod">
+      <v-list-item-group @change="onChangeSelectedMod" color="#a8cded">
         <Mod
           v-for="(mod, i) in mods"
           :key="mod.id"
