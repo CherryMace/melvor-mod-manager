5908f2b0cdb4254f5e53d9c3de27ed91d8d0ee56
Chase Strackbein <chasestrackbein@gmail.com>
2021-09-09 17:38:39 -0400

    Inject at page load rather than on game load

--- src/modsHandler.js
+++ src/modsHandler.js
@@ -411,51 +411,38 @@ const buildM3Js = (mods) => {
     modInjectables.push(`{ id: '${mod.id}', scripts: ${JSON.stringify(mod.entryScripts || [])}, styles: ${JSON.stringify(mod.styles || [])} }`);
   }
   return `
-(() => {
-  const load = () => {
-    const isGameLoaded = window.isLoaded && !window.currentlyCatchingUp;
-      
-    if (!isGameLoaded) {
-      setTimeout(load, 50);
-      return;
+window.addEventListener('load', () => {
+  if (!document.getElementById('m-page-loader')) return;
+
+  const fs = require('fs');
+  const path = require('path');
+
+  const runtimeGetURL = chrome.runtime.getURL;
+  chrome.runtime.getURL = (path, modId) => {
+    if (modId) path = modId + path;
+    return runtimeGetURL(path);
+  };
+
+  const mods = [
+    ${modInjectables.join(',\n      ')}
+  ];
+  
+  for (const mod of mods) {
+    for (const script of mod.scripts) {
+      const filePath = path.join('Mods', mod.id, script);
+      fs.readFile(filePath, 'utf8', (err, content) => {
+        const scriptEl = document.createElement('script');
+        scriptEl.innerHTML = \`(() => { \${content} })();\`;
+        document.body.appendChild(scriptEl);
+      });
     }
- 
-    inject();
-  }
- 
-  const inject = () => {
-    const fs = require('fs');
-    const path = require('path');
-
-    const runtimeGetURL = chrome.runtime.getURL;
-    chrome.runtime.getURL = (path, modId) => {
-      if (modId) path = modId + path;
-      return runtimeGetURL(path);
-    };
-
-    const mods = [
-      ${modInjectables.join(',\n      ')}
-    ];
-    
-    for (const mod of mods) {
-      for (const script of mod.scripts) {
-        const filePath = path.join('Mods', mod.id, script);
-        fs.readFile(filePath, 'utf8', (err, content) => {
-          const scriptEl = document.createElement('script');
-          scriptEl.innerHTML = \`(() => { \${content} })();\`;
-          document.body.appendChild(scriptEl);
-        });
-      }
-      for (const style of mod.styles) {
-        const filePath = path.join('Mods', mod.id, style);
-        const linkEl = document.createElement('link');
-        linkEl.rel = 'stylesheet';
-        linkEl.href = chrome.runtime.getURL(filePath);
-        document.head.appendChild(linkEl);
-      }
+    for (const style of mod.styles) {
+      const filePath = path.join('Mods', mod.id, style);
+      const linkEl = document.createElement('link');
+      linkEl.rel = 'stylesheet';
+      linkEl.href = chrome.runtime.getURL(filePath);
+      document.head.appendChild(linkEl);
     }
   }
- 
-  load();
-})();`
+});`
 };
